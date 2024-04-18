c0151a61cc8fb73dc802c79a0c2bdd2d85b48e51
sreyaudhayakumar <sreyasreya43@gmail.com>
2024-04-18 17:49:44 +0530

    corrected error

--- rest_app/serializer.py
+++ rest_app/serializer.py
@@ -7,8 +7,14 @@ class RoleSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 class UserSerializer(serializers.ModelSerializer):
-    role = RoleSerializer()  
+    role = RoleSerializer()
 
     class Meta:
         model = User
         fields = ['id', 'username', 'age', 'status', 'role']
+
+    def create(self, validated_data):
+        role_data = validated_data.pop('role')
+        role_instance = Role.objects.create(**role_data)
+        user = User.objects.create(role=role_instance, **validated_data)
+        return user
